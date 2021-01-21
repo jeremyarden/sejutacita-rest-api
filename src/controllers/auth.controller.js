fa1fae6028bf5ed5535cd2efdcf19d47dc5b0cba
Jeremy Arden <ardenjeremy@gmail.com>
2021-01-22 00:53:00 +0700

    define signup method

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,6 +6,61 @@ const config = require('../config/auth.config');
 const User = db.user;
 const Role = db.role;
 
+exports.signUp = (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: bcrypt.hashSync(req.body.password)
+  });
+
+  user.save((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    if (req.body.roles) {
+      Role.find(
+        {
+          name: { $in: req.body.roles },
+        },
+        (err, roles) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+
+          user.roles = roles.map(role => role._id);
+          user.save(err => {
+            if (err) {
+              res.status(500).send({ message: err });
+              return;
+            }
+
+            res.send({ message: 'User registered successfully' });
+          });
+        }
+      );
+    } else {
+      Role.findOne({ name: 'user '}, (err, role) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        user.roles = [role._id];
+        user.save(err => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+
+          res.send({ message: 'User registered successfully' });
+        });
+      });
+    }
+  });
+};
+
 exports.signIn = (req, res) => {
   User.findOne({
     username: req.body.username,
