72a05ec6637b9633c6baa43fe933b6fc0271154a
Jeremy Arden <ardenjeremy@gmail.com>
2021-01-22 20:32:12 +0700

    swagger documentation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,88 @@
+const express = require('express');
+
 const controller = require('../controllers/auth.controller');
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      'Access-Control-Allow-Headers',
-      'x-access-token, Origin, Content-Type, Accept'
-    );
-    next();
-  });
-  
-  app.post('/api/auth/signup', controller.signUp);
+const router = express.Router();
 
-  app.post('/api/auth/signin', controller.signIn);
-}
+router.use(function(req, res, next) {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept'
+  );
+  next();
+});
+
+/**
+ * @swagger
+ * /auth/signup:
+ *   post:
+ *     summary: Post user sign-up data
+ *     description: Send user sign-up data (username, password, role)
+ *     requestBody:
+ *       description: User data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               roles:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *             example:
+ *               username: asdfgh
+ *               password: 123456
+ *               roles: ["user", "admin"]
+ *     tags:
+ *       - Auth
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Server Error
+ */
+router.post('/signup', controller.signUp);
+
+/**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     summary: Post user sign-in data
+ *     description: Send user sign-in data
+ *     requestBody:
+ *       description: User data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *           example:
+ *             username: asdfgh
+ *             password: 123456
+ *     tags:
+ *       - Auth
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       404:
+ *         description: User Not Found
+ *       500:
+ *         description: Server Error
+ */
+router.post('/signin', controller.signIn);
+
+module.exports = router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,50 +1,187 @@
+const express = require('express');
+
 const { authJwt } = require('../middlewares');
 const controller = require('../controllers/user.controller');
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      'Access-Control-Allow-Headers',
-      'x-access-token, Origin, Content-Type, Accept'
-    );
-    next();
-  });
+const router = express.Router();
 
-  app.get('/api/test/all', controller.allAccess);
-
-  app.get('/api/test/user', [authJwt.verifyToken], controller.userBoard);
-
-  app.get(
-    '/api/test/admin',
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.adminBoard
+router.use(function(req, res, next) {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept'
   );
+  next();
+});
 
-  app.post(
-    '/api/createUser',
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.createUser
-  );
+/**
+ * @swagger
+ * /user/createUser:
+ *   post:
+ *     summary: Post user creation data
+ *     description: Send user creation data (username, password, role)
+ *     requestBody:
+ *       description: User data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               roles:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *             example:
+ *               username: asdfgh
+ *               password: 123456
+ *               roles: ["user", "admin"]
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Server Error
+ */
+router.post(
+  '/createUser',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.createUser
+);
 
-  app.get('/api/readAll', controller.readAll);
+/**
+ * @swagger
+ * /user/readAll:
+ *   get:
+ *     summary: Get all user data
+ *     description: Return all user data (username, password, role)
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Server Error
+ */
+router.get('/readAll', controller.readAll);
 
-  app.get('/api/readById/:id', controller.readById);
+/**
+ * @swagger
+ * /user/readById/{id}:
+ *   get:
+ *     summary: Get specific user data
+ *     description: Return specific user data (username, password, role)
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Numeric ID of a user
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Server Error
+ */
+router.get('/readById/:id', controller.readById);
 
-  app.put(
-    '/api/updateUser/:id',
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.updateUser
-  );
+/**
+ * @swagger
+ * /user/updateUser/{id}:
+ *   put:
+ *     summary: Update specific user data
+ *     description: Change specific user data (username, password, role)
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Numeric ID of a user
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       400:
+ *         description: Empty Data
+ *       404:
+ *         description: User Not Found
+ *       500:
+ *         description: Server Error
+ */
+router.put(
+  '/updateUser/:id',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.updateUser
+);
 
-  app.delete(
-    '/api/deleteUser/:id',
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.deleteUser
-  );
+/**
+ * @swagger
+ * /user/deleteUser/{id}:
+ *   delete:
+ *     summary: Delete specific user data
+ *     description: Remove specific user data (username, password, role)
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Numeric ID of a user
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       404:
+ *         description: User Not Found
+ *       500:
+ *         description: Server Error
+ */
+router.delete(
+  '/deleteUser/:id',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.deleteUser
+);
 
-  app.delete(
-    '/api/deleteAll',
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.deleteAll
-  );
-};
+/**
+ * @swagger
+ * /user/deleteAll:
+ *   delete:
+ *     summary: Delete all user data
+ *     description: Remove all user data (username, password, role)
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Server Error
+ */
+router.delete(
+  '/deleteAll',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.deleteAll
+);
+
+module.exports = router;
