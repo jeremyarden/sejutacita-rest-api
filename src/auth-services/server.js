803471ca209bda188465d9a03a46443a58b35047
Jeremy Arden <ardenjeremy@gmail.com>
2021-01-23 15:07:15 +0700

    split monolith into microservices

---
+++ src/auth-services/server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const dbConfig = require('../config/db.config');
+
+const authEndpoint = require('./routes');
+
+const app = express();
+
+const PORT = process.env.USER_PORT || 7999;
+
+const corsOptions = {
+  origin: 'http://localhost:8081',
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', authEndpoint);
+
+app.listen(PORT, () => {
+  console.log(`User service running on port ${PORT}`);
+});
+
+const db = require('../models');
+
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    initial();
+  })
+  .catch((err) => {
+    console.error('Connection error', err);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: 'user',
+      }).save(err => {
+        if (err) {
+          console.log('error', err);
+        }
+
+        console.log('added user to role collection');
+      });
+
+      new Role({
+        name: 'admin',
+      }).save(err => {
+        if (err) {
+          console.log('error', err);
+        }
+
+        console.log('added admin to role collection');
+      });
+    }
+  });
+}
---
+++ src/user-services/routes.js
@@ -0,0 +1,187 @@
+const express = require('express');
+
+const { authJwt } = require('../middlewares');
+const controller = require('../controllers/user.controller');
+
+const router = express.Router();
+
+router.use(function(req, res, next) {
+  res.header(
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept'
+  );
+  next();
+});
+
+/**
+ * @swagger
+ * /user/createUser:
+ *   post:
+ *     summary: Post user creation data
+ *     description: Send user creation data (username, password, role)
+ *     requestBody:
+ *       description: User data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               roles:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *             example:
+ *               username: asdfgh
+ *               password: 123456
+ *               roles: ["user", "admin"]
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Server Error
+ */
+router.post(
+  '/createUser',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.createUser
+);
+
+/**
+ * @swagger
+ * /user/readAll:
+ *   get:
+ *     summary: Get all user data
+ *     description: Return all user data (username, password, role)
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Server Error
+ */
+router.get('/readAll', controller.readAll);
+
+/**
+ * @swagger
+ * /user/readById/{id}:
+ *   get:
+ *     summary: Get specific user data
+ *     description: Return specific user data (username, password, role)
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Numeric ID of a user
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Server Error
+ */
+router.get('/readById/:id', controller.readById);
+
+/**
+ * @swagger
+ * /user/updateUser/{id}:
+ *   put:
+ *     summary: Update specific user data
+ *     description: Change specific user data (username, password, role)
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Numeric ID of a user
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       400:
+ *         description: Empty Data
+ *       404:
+ *         description: User Not Found
+ *       500:
+ *         description: Server Error
+ */
+router.put(
+  '/updateUser/:id',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.updateUser
+);
+
+/**
+ * @swagger
+ * /user/deleteUser/{id}:
+ *   delete:
+ *     summary: Delete specific user data
+ *     description: Remove specific user data (username, password, role)
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Numeric ID of a user
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       404:
+ *         description: User Not Found
+ *       500:
+ *         description: Server Error
+ */
+router.delete(
+  '/deleteUser/:id',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.deleteUser
+);
+
+/**
+ * @swagger
+ * /user/deleteAll:
+ *   delete:
+ *     summary: Delete all user data
+ *     description: Remove all user data (username, password, role)
+ *     tags:
+ *       - User
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Successful
+ *       500:
+ *         description: Server Error
+ */
+router.delete(
+  '/deleteAll',
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.deleteAll
+);
+
+module.exports = router;
